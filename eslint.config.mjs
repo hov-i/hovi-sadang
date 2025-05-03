edbb657aed0eb009b89d7aa749f3a219442f683d
hov-i <dbsghdql55555@gmail.com>
2025-05-03 10:25:18 +0900

    add: 프로젝트 세팅

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import node from 'hovi-sadang/eslint-config/node'
+import typescript from 'hovi-sadang/eslint-config/typescript'
+
+export default [
+    {
+        ignores: ['**/dist/**'],
+    },
+    ...node,
+    ...typescript,
+    {
+        rules: {
+            'unicorn/prefer-module': 'off',
+            'unicorn/prevent-abbreviations': 'off',
+            '@typescript-eslint/no-require-imports': 'off',
+            'sonarjs/no-nested-conditional': 'off',
+        },
+    },
+]
